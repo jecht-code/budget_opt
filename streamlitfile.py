ad7c400901de2d51d80e6b8793bd5e5cb2142860
Scott Pak <gpak@saintpeters.edu>
2024-12-29 23:14:46 -0500

    added a filter drop down on brand level

--- streamlitfile.py
+++ streamlitfile.py
@@ -5,6 +5,16 @@ st.title('budget optimizer')
 
 st.write('Hellow World!')
 
+#Show case DataFrame - to ensure data is ingested into Streamlit.
 df = pd.read_csv('DataSet_Streamlit.csv')
 
-df
+#Make a drop down that filters said dataframe.
+# Create the dropdown
+options = df['Brand'].unique().tolist()
+selected_category = st.selectbox('Select Brand:', options)
+
+# Filter the DataFrame
+filtered_df = df[df['Brand'] == selected_category]
+
+# Display the filtered DataFrame
+st.dataframe(filtered_df)
